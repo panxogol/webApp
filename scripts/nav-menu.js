92fd42dac53f0a6d9abb25422443b53e8e84cde3
Juan Martinez <juan.martinez.p@usach.cl>
2020-05-09 21:06:44 -0400

    configuring the text nav-menu in animation

--- scripts/nav-menu.js
+++ scripts/nav-menu.js
@@ -1,11 +1,19 @@
 const navSlide = () => {
     const burguer = document.querySelector('.burger');
     const nav = document.querySelector('.nav-menu');
-
+    const navLinks = document.querySelectorAll('.nav-menu li');
+    
+    // Toggle Nav
     burguer.addEventListener('click', () => {
-        console.log("click en burger");
         nav.classList.toggle('nav-menu-active');
-    })
+    });
+    
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        console.log(index / 7 + 1.5);
+        link.style.animation = "navLinkFade 0.5s ease forwards ${index / 5 + 1.5}s";
+    });
+
 }
 
 navSlide();
