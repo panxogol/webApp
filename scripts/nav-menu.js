2ce01b6f85e0315b5c072530c40d850eac5890bd
Juan Martinez <juan.martinez.p@usach.cl>
2020-05-10 22:04:54 -0400

    Responsive for celphones try 1

--- scripts/nav-menu.js
+++ scripts/nav-menu.js
@@ -2,19 +2,22 @@ const navSlide = () => {
     const burguer = document.querySelector('.burger');
     const nav = document.querySelector('.nav-menu');
     const navLinks = document.querySelectorAll('.nav-menu li');
-    
+
     burguer.addEventListener('click', () => {
         // Toggle Nav
         nav.classList.toggle('nav-menu-active');
-        
+
         //Animate Link
         navLinks.forEach((link, index) => {
+            link.style.opacity = '0';
             var delay = index / 5 + 0.6;
             var animationIn = 'navLinkFade 0.5s ease forwards ' + delay + 's';
             var animationOut = 'navLinkFadeOut 0.5s';
+
             if (link.style.animation) {
                 link.style.animation = animationOut;
-                setTimeout(function(){link.style.animation = '';}, 500);
+                setTimeout(function () { link.style.animation = ''; }, 500);
+                link.style.opacity = '1';
             }
 
             else {
